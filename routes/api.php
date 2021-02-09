52555b8d19d643ba494def642dee8dd7cd44b87e
romel diaz <root.romel@gmail.com>
2021-02-09 17:41:53 -0500

    configuracion de arranque de servicio

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/service/print/reports', 'ServiceController@printReport');
 Route::post('/service/print/shipping', 'ServiceController@printShipping');
-
-
-Route::get('/free', function(){
-    return 'free route';
-});
